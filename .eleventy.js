304779aba45c4d9886737fbc73290c94592904a3
Abdulla Almansoori <killerfrankie970@gmail.com>
2026-05-06 17:54:31 +0300

    set dark mode as the default mode, added code syntax feature

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/favicon.png");
   eleventyConfig.addPassthroughCopy("src/assets");
+
+  eleventyConfig.addPlugin(syntaxHighlight);
+
   return {
     dir: {
       input: "src",
---
+++ src/portfolio/game-ui.md
@@ -0,0 +1,12 @@
+---
+layout: project.njk
+title: Game UI
+pageTitle: Game UI
+pageSlug: portfolio/game-ui
+parentTitle: Portfolio
+parentSlug: portfolio
+---
+```js
+const hello = "world";
+console.log(hello);
+```
