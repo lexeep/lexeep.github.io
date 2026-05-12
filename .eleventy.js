46ab9f14ca8964efcc29108ffed5b48edef14381
Abdulla Almansoori <killerfrankie970@gmail.com>
2026-05-12 15:25:52 +0300

    added node_modules to .gitignore

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,19 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-module.exports = function (eleventyConfig) {
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
+
+export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/favicon.png");
   eleventyConfig.addPassthroughCopy("src/assets");
-
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
+    viteOptions: {
+      server: {
+        middlewareMode: false,
+      },
+    },
+  });
 
   return {
     dir: {
@@ -12,4 +21,4 @@ module.exports = function (eleventyConfig) {
       output: "_site",
     },
   };
-};
+}
